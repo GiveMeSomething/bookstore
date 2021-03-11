bdb59e54efd412952044ced6df6d1071e210711b
Hoang Minh <htminh.1001@gmail.com>
2021-03-11 17:49:36 +0700

    feat: add new (add addr page) + add func to retrieve City, District, SubDistrict from DB

--- src/java/address/Address_Controller.java
+++ src/java/address/Address_Controller.java
@@ -31,10 +31,12 @@ public class Address_Controller extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
     }
 }
--- src/java/address/Address_Service.java
+++ src/java/address/Address_Service.java
@@ -1,14 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package address;
 
-/**
- *
- * @author Admin
- */
+import entities.address.City;
+import entities.address.District;
+import entities.address.SubDistrict;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class Address_Service {
 
+    private final Address_Repository address_Repository;
+
+    public Address_Service() {
+        this.address_Repository = new Address_Repository();
+    }
+
+    public ArrayList<City> getCity() {
+        ArrayList<City> cityList;
+        try {
+            cityList = this.address_Repository.getCity();
+            return cityList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<District> getDistrict() {
+        ArrayList<District> districtList;
+        try {
+            districtList = this.address_Repository.getDistrict();
+            return districtList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<SubDistrict> getSubDistricts() {
+        ArrayList<SubDistrict> subDistrictList;
+        try {
+            subDistrictList = this.address_Repository.getSubDistricts();
+            return subDistrictList;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
